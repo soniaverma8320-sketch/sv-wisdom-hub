8bf51c66d5b6f00fc2a800cbf7c08ac92c72e006
Soniya Verma <soniaverma8320@gmail.com>
2026-01-18 16:53:43 +0530

    Move src & public to root, add _redirects, fix SPA routing

---
+++ sv-wisdom-hub/src/pages/Admin/index.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Admin from "./pages/admin";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
