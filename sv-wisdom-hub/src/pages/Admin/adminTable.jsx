7ff03d7ec2a9e3e621ccf71ef5097c1f5e297294
Soniya Verma <soniaverma8320@gmail.com>
2026-01-18 15:53:16 +0530

    Add src folder (React app)

---
+++ sv-wisdom-hub/src/pages/Admin/adminTable.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Admin from "./pages/admin";
+import Home from "./pages/home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
